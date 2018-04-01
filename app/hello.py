a07de1784fe12b5548dd6dd7fba91e00d5b266d0
prasanna158 <prasannavenky333@gmail.com>
2018-04-02 00:02:26 +0530

    dv

--- app/hello.py
+++ app/hello.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/',methods =['POST'])
 
 def home():
-    req = request.json()
+    req = request.get_json(silent=True,force=True)
     recipe = req.get("result").get("parameters").get("recipe")
     response = requests.get("https://api.edamam.com/search?q="+ recipe+"&app_id=609ddfda&app_key=3b418fd7c775e74b7bf933b67b8b645f&from=0&to=3")
     res = response.json()

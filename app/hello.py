8801df0f00250ea1ba1db7344e99562636f800e0
prasanna158 <prasannavenky333@gmail.com>
2018-04-01 23:44:12 +0530

    dv

--- app/hello.py
+++ app/hello.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def home():
     req = request.json()
     recipe = req.get("result").get("parameters").get("recipe")
-    response = requests.get("https://api.edamam.com/search?q="+ recipe+"&app_id=609ddfda&app_key=3b418fd7c775e74b7bf933b67b8b645f&from=0&to=3")
+    response = req.get("https://api.edamam.com/search?q="+ recipe+"&app_id=609ddfda&app_key=3b418fd7c775e74b7bf933b67b8b645f&from=0&to=3")
     res = response.json()
     speech="testdata"
     
@@ -19,7 +19,7 @@ def home():
         "displayText":speech,
         "source":"dialogflowResponse"
             }, indent=4)
-            
+
     r = make_response(resul)
     return r
 

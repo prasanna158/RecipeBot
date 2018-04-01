022f0f3394048f5f2bbcf7c0109ef49aec2333a2
prasanna158 <prasannavenky333@gmail.com>
2018-04-02 00:14:07 +0530

    dv

--- app/hello.py
+++ app/hello.py
@@ -11,9 +11,7 @@ def home():
     recipe = req.get("result").get("parameters").get("recipe")
     response = requests.get("https://api.edamam.com/search?q="+ recipe+"&app_id=609ddfda&app_key=3b418fd7c775e74b7bf933b67b8b645f&from=0&to=3")
     res = response.json()
-    speech="testdata"
-    
-
+    speech=res.get("hits")[0].get("recipe").get("url")
     resul = json.dumps({
         "speech":speech,
         "displayText":speech,

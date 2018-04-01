4aa112f772f9e32aa36e41636d29f4866e142ce6
prasanna158 <prasannavenky333@gmail.com>
2018-04-01 23:31:24 +0530

    cvd

--- app/hello.py
+++ app/hello.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask,request,make_response
 import json
 
@@ -6,23 +7,20 @@ app = Flask(__name__)
 @app.route('/',methods =['POST'])
 
 def home():
-    
-    #response = requests.get("https://api.edamam.com/search?q="+ rec +"&app_id=609ddfda&app_key=3b418fd7c775e74b7bf933b67b8b645f&from=0&to=3")
-    ##res = response.json()
-
-    req = request.get_json(silent=True,force=True)
-    print("hello")
-    print(json.dumps(req,indent =4))
-
+    req = request.json()
+    recipe = req.get("result").get("parameters").get("recipe")
+    response = requests.get("https://api.edamam.com/search?q="+ recipe+"&app_id=609ddfda&app_key=3b418fd7c775e74b7bf933b67b8b645f&from=0&to=3")
+    res = response.json()
     speech="testdata"
     
 
-    res = json.dumps({
+    resul = json.dumps({
         "speech":speech,
         "displayText":speech,
         "source":"dialogflowResponse"
             }, indent=4)
-    r = make_response(res)
+            
+    r = make_response(resul)
     return r
 
 if __name__ == '__main__':
